2b377e6b8848956c8bb61f2a86e1cd69881b9c25
wilmot_g <guillaume.wilmot@epitech.eu>
2017-07-27 19:08:44 +1000

    Killed the drawer.

--- app/src/main/java/com/poncholay/bigbrother/activities/BigBrotherActivity.java
+++ app/src/main/java/com/poncholay/bigbrother/activities/BigBrotherActivity.java
@@ -1,14 +1,9 @@
 package com.poncholay.bigbrother.activities;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -20,34 +15,19 @@ import com.poncholay.bigbrother.controllers.DynamicTitledFragmentPagerAdapter;
 
 import me.relex.circleindicator.CircleIndicator;
 
-public class BigBrotherActivity extends AppCompatActivity
-		implements NavigationView.OnNavigationItemSelectedListener {
+public class BigBrotherActivity extends AppCompatActivity {
 
 	private DynamicTitledFragmentPagerAdapter mAdapter;
-	private Context mContext;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_big_brother);
 
-		mContext = this;
-
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_big_brother);
 		setSupportActionBar(toolbar);
 
-		setupMenu(toolbar);
 		setupPager(savedInstanceState);
-	}
-
-	@Override
-	public void onBackPressed() {
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-		if (drawer.isDrawerOpen(GravityCompat.START)) {
-			drawer.closeDrawer(GravityCompat.START);
-		} else {
-			super.onBackPressed();
-		}
 	}
 
 	@Override
@@ -68,46 +48,10 @@ public class BigBrotherActivity extends AppCompatActivity
 		return super.onOptionsItemSelected(item);
 	}
 
-	@SuppressWarnings("StatementWithEmptyBody")
-	@Override
-	public boolean onNavigationItemSelected(MenuItem item) {
-		// Handle navigation view item clicks here.
-		int id = item.getItemId();
-
-		if (id == R.id.nav_camera) {
-			// Handle the camera action
-		} else if (id == R.id.nav_gallery) {
-
-		} else if (id == R.id.nav_slideshow) {
-
-		} else if (id == R.id.nav_manage) {
-
-		} else if (id == R.id.nav_share) {
-
-		} else if (id == R.id.nav_send) {
-
-		}
-
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-		drawer.closeDrawer(GravityCompat.START);
-		return true;
-	}
-
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		outState.putParcelable("pagerState", mAdapter.saveState());
 		super.onSaveInstanceState(outState);
-	}
-
-	private void setupMenu(Toolbar toolbar) {
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-		drawer.addDrawerListener(toggle);
-		toggle.syncState();
-
-		NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-		navigationView.setNavigationItemSelectedListener(this);
 	}
 
 	private void setupPager(Bundle bundle) {
